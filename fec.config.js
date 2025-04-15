e36fdb12302832e86d4528353ad0573e0ed17643
Matej Marusak <marusak.matej@gmail.com>
2025-04-15 13:59:25 +0200

    Cleanup fec config
    <Insert "I have no idea what I am doing" meme>

--- fec.config.js
+++ fec.config.js
@@ -1,40 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const { dependencies, insights } = require('./package.json');
 
-const moduleName = insights.appname.replace(/-(\w)/g, (_, match) => match.toUpperCase());
 const srcDir = path.resolve(__dirname, './src');
 
 module.exports = {
-  appName: moduleName,
+  sassPrefix: '.contentSources',
+  debug: true,
+  useFileHash: true,
+  devtool: 'hidden-source-map',
   appUrl: '/insights/content/repositories',
   useProxy: true,
+  useAgent: true,
+  bounceProd: false,
+  proxyVerbose: true,
   moduleFederation: {
-    moduleName,
     exposes: {
       './RootApp': path.resolve(__dirname, './src/AppEntry.tsx'),
     },
-    shared: [
-      {
-        'react-router-dom': {
-          singleton: true,
-          import: false,
-          version: dependencies['react-router-dom'],
-          requiredVersion: '>=6.0.0 <7.0.0',
-        },
-      },
-      {
-        '@unleash/proxy-client-react': {
-          version: dependencies['@unleash/proxy-client-react'],
-          singleton: true,
-        },
-      },
-    ],
+    shared: [{ 'react-router-dom': { singleton: true, version: '*' } }],
+    exclude: ['react-router-dom']
   },
-  /**
-   * Add additional webpack plugins
-   */
-  //   plugins: [...(process.env.VERBOSE ? [new WatchRunPlugin()] : []), new webpack.ProgressPlugin()],
   resolve: {
     modules: [srcDir, path.resolve(__dirname, './node_modules')],
   },
